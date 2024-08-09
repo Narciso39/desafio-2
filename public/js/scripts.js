091450aba7f4dab74664da6366c03c9728ce6d54
luis <felipenarciso15@gmail.com>
2024-08-09 00:55:29 -0300

    mensagem de promoção terminada

---
+++ public/js/scripts.js
@@ -0,0 +1,7 @@
+const buttonClose = document.querySelector(".closeBtn");
+
+// função para remover a mensagem de promoção
+buttonClose.addEventListener("click", () => {
+    document.getElementById("promoHeader").remove();
+});
+
