b844221c32c5a7bbfa87e6ab81a8de3a1ef3b1b5
luis <felipenarciso15@gmail.com>
2024-08-09 23:29:10 -0300

    header web e mobile finalizado

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,12 @@
 const buttonClose = document.querySelector(".closeBtn");
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
 
 // função para remover a mensagem de promoção
 buttonClose.addEventListener("click", () => {
-    document.getElementById("promoHeader").remove();
+  document.getElementById("promoHeader").remove();
+});
+//função para adicionar e remover a lista no menu hamburger
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("active");
 });
