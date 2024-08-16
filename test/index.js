ec2f9ea24371c176df51c3b025ac0a6c27ce440e
luis <felipenarciso15@gmail.com>
2024-08-15 21:40:33 -0300

    branch do mobile criada

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-const copyright = document.querySelector(".infoFooterP");
-const year =  new Date();
-const currentYear = year.getFullYear();
-copyright.innerHTML = `Shop.co © 2000-${currentYear}, All Rights Reserved`
+// const copyright = document.querySelector(".infoFooterP");
+// const year =  new Date();
+// const currentYear = year.getFullYear();
+// copyright.innerHTML = `Shop.co © 2000-${currentYear}, All Rights Reserved`
